2a3f023bfa9fe99a57ba09c8c5509d1991a47293
George Lydecker <george.lydecker@wmg.com>
2024-10-31 09:35:19 -0700

    Update ChaseLights.ino
    Update comment on initializing digital output pins

--- ChaseLights.ino
+++ ChaseLights.ino
@@ -10,7 +10,7 @@ int offTime = 10 ;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pins 2, 3 & 4 as an output.
   pinMode(2, OUTPUT); //LED 1
   pinMode(3, OUTPUT); //LED 2
   pinMode(4, OUTPUT); //LED 3
